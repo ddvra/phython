d1127c52fb7a3641bc9c1f891958a1f1b6ea862d
ddvra <wonwoo622@gmail.com>
2017-06-27 17:14:36 +0900

    Add files via upload

---
+++ 주식.py
@@ -0,0 +1,30 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> daum = 89000
+>>> daum*100
+8900000
+>>> pont up
+SyntaxError: invalid syntax
+>>> naver=751000
+>>> print(daum*100+naver*20)
+23920000
+>>> daum=daum*(1-0.05)
+>>> daum
+84550.0
+>>> naver=naver*(1-0.01)
+>>> naver
+743490.0
+>>> naver=890000
+]
+>>> naver=890000
+>>> naver=naver*(1-0.1)
+>>> naver
+801000.0
+>>> print(naver*20+daum*)
+SyntaxError: invalid syntax
+>>> print(naver*20+daum*100)
+24475000.0
+>>> s1=daum*100+naver*20
+>>> s1
+24475000.0
+>>> 
